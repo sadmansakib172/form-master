26ff477db193f91acdd42d1702a857c9741703d6
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-22 11:46:37 +0600

    name, email type input field value getting done

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -2,12 +2,16 @@
 const SimpleForm = () => {
     const handleSubmit = e =>{
         e.preventDefault()
+        console.log(e.target.name.value);
+        console.log(e.target.email.value)
         console.log('form submitted');
     }
     return (
         <div>
             <form onSubmit={handleSubmit} >
-                <input type="text" />
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
