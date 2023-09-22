d3efa785f491337b02bb699fd705074c6ccd51e0
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-22 13:17:38 +0600

    controlled and uncontrolled form learning done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
-import SimpleForm from './components/SimpleForm/SimpleForm'
-import StatefulForm from './components/StatefulForm/StatefulForm'
+import RefForm from './components/RefForm/RefForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
