aca47230ac54c7453e8b122c76192b0df21a232c
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-22 11:34:26 +0600

    basic form submitted

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
 
 const SimpleForm = () => {
+    const handleSubmit = e =>{
+        e.preventDefault()
+        console.log('form submitted');
+    }
     return (
         <div>
-            
+            <form onSubmit={handleSubmit} >
+                <input type="text" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
